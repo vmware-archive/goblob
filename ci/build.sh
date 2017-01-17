6a9df3f1f118228d38fdfad5a4b56dd4dac8adcb
Kris Hicks <khicks@pivotal.io>
2017-01-17 10:57:53 -0800

    Cross-compile release binaries
    [#137314071]

---
+++ ci/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+-ex
+
+export GOPATH=$PWD/go
+
+ROOT=$(cd $(dirname $0) && pwd)
+version=$(cat $ROOT/version/version)
+
+cd go/src/github.com/pivotalservices/goblob
+  go build \
+    -o $BINARY_FILENAME github.com/pivotalservices/goblob/cmd/goblob
+    -ldflags "-X goblob.Version=${version}"
+cd -
